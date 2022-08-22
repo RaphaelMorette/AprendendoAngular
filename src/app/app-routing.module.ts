d692e45e0481aa162cec7cdcef4833abfdbf42fc
RaphaelMorette <raphael.morette@foursys.com>
2022-08-22 08:41:58 -0300

    Aula 12 completa

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { FirstComponentComponent } from "./components/first-component/first-component.component";
+import { IfRenderComponent } from "./components/if-render/if-render.component";
+import { ListRenderComponent } from "./components/list-render/list-render.component";
+
+const routes: Routes = [
+    {path: '', component: FirstComponentComponent},
+    {path: 'list', component: ListRenderComponent},
+    {path: 'if', component: IfRenderComponent}
+]
+
+@NgModule({
+    declarations: [],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
